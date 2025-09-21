e1fa80e10bc5481b5eb3235ca4f0ea6a472c066a
hiclassicm <hares.ahmed.319@gmail.com>
2025-09-21 20:45:53 +0600

    component

---
+++ importexport/component.js
@@ -0,0 +1,17 @@
+export let a = 45;
+export let str = "hiclassic"
+export let obj = {
+    name: "Rahim", age: 27
+}
+
+export let arr = [1, 2, 3, 4]
+
+export let hello = () => {
+    console.log("Hello World");
+}
+
+export class Person {
+    constructor() {
+        this.name = "Amin"
+    }
+}
---
+++ importexport/index.js
@@ -0,0 +1,10 @@
+import { str, a, arr, hello, obj, Person } from './component.js';
+
+
+console.log(str);
+console.log(obj);
+hello();
+
+let person = new Person();
+
+console.log(person);  
